83f3596987685940b9d64000a04838d829b5842b
Kylie Anyce Bearden <kylie.bearden@vumc.com>
2022-02-23 14:37:14 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿System.Console.Write("Who would you like to say hello to?");
+
+string name = System.Console.ReadLine();
+
+if (string.IsNullOrWhiteSpace(name))
+{
+    System.Console.WriteLine("Fine, don't say 'hello'!");
+}
+else
+{
+    System.Console.WriteLine($"Hello, {name}!");
+}
